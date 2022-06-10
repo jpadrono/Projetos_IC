40af42979428c5f3d5afbb72b9f8c4a7abb57bff
jpadrono <101663774+jpadrono@users.noreply.github.com>
2022-06-09 23:13:34 -0300

    Update controle_de_acesso_ao_IME.c

--- controle_de_acesso_ao_IME.c
+++ controle_de_acesso_ao_IME.c
@@ -7,6 +7,8 @@
 /******************************************************************************
  *                              COMENTARIOS                                   *
  * 1.Achar um nome melhor para "TIPO"                                         *
+ * 2.validacao do cpf l.120                                                   *
+ * 3.confirmar os postos de oficiais                                          *
  * ****************************************************************************/
 //O codigo trabalha com lista encadeada
 char postos[] = "Asp ofTenCapMajTCelGen brig Gen Div Gen Ex";
@@ -19,7 +21,7 @@ typedef struct _controle{
     char destino[20];
     char hora_entrada[6];
     char hora_saida[6];
-    //ponteiros auxiliares para listagem e linkagem dos dados
+    //ponteiros auxiliares para listagem encadeada dos dados
     struct _controle *prox;
     struct _controle *ant;
 } controle;
@@ -27,7 +29,7 @@ typedef struct _controle{
 
 controle *ptr_inicio = NULL;
 
-//funcoes que serao utilizadas
+//funcoes e variaveis que serao utilizadas
 void salvar(controle *ptr, FILE* aqr);
 void inserir(controle **ptr);
 void listar();
@@ -39,11 +41,22 @@ void sort_data();
 int data_ord[100], ord, n=0;
 
 int main(){
+    //obviamente n vai deixar assim, coloquemo nome, TAB, e simbolos que quiserem
     p("\t\tTELA BONITA DE CADASTRO\t\t\n");
+    //menu que sera repetido ate a escolha da opcao N
     do{
-        inserir(&ptr_inicio);
+        //para saber quando o do while eh repitido por escolha invalida
+        //se valid != 0 o usuario escolheu uma opcao inexistente
+        int valid = 0;
+        // o ponteiro ptr_inicio eh a base da nossa lista encadeada
+        if(!valid--) inserir(&ptr_inicio);
         p("\nDeseja cadastrar mais uma pessoa (S/N)?: ");
         s(" %c", &op);
+        if(op != 's' && op != 'S' && op != 'n' && op != 'N'){
+            p("Opcao invalida!\n");
+            valid++;
+            op = 's';
+        }
     } while(op == 's' || op == 'S');
 
     listar(ptr_inicio);
@@ -52,7 +65,8 @@ int main(){
 }
 
 void inserir(controle **ptr){
-
+    // ponteiro de controle para ordenar a lista encadeada
+    // as variaveis serao ordenadas no momento da criacao
     controle *n_ptr, *o_ptr, *ptr_aux;
 
     int ord;
@@ -64,6 +78,9 @@ void inserir(controle **ptr){
     ptr_aux = *ptr;
     o_ptr = NULL;
 
+    //leitura dos valores do cadastrado
+    //as validacoes ainda precisao ser feitas
+    //validacao do cpf eh obrigatoria, mas eh interessante fazer das outras
     char var[100];
     for(int i = 0; i < 7; i++){
         switch(i){
@@ -80,6 +97,8 @@ void inserir(controle **ptr){
             case 2:
                 p("Digite a Data no formato dd/mm/aaaa: ");
                 s(" %d/%d/%d", &n_ptr->dia, &n_ptr->mes, &n_ptr->ano);
+                //como a ordenacao eh feita pelas data, criei uma variavel ord
+                //como um inteiro, vacilitando a comparacao das variaveis
                 ord = n_ptr->dia + n_ptr->mes*100 + n_ptr->ano*10000;
                 n_ptr->ord = ord;
                 break;
@@ -99,26 +118,33 @@ void inserir(controle **ptr){
                 strcpy(n_ptr->destino, var);
                 break;
             case 6:
+                //fazer um do while para loopar o scan do cpf, o processo eh
+                //repetido enquanto o formato estiver errado
                 p("Digite seu CPF no formato XXXXXXXXX-XX: ");
                 s(" %[^\n]", var);
+                //}while(validacaoc_cpf());
                 strcpy(n_ptr->cpf, var);
                 break;
         }
     }
 
+    //caso seja o primeiro cadastro do arquivo
     if(ptr_aux == NULL){
         n_ptr->prox = NULL;
         *ptr = n_ptr;
     }
     else{
+        //mesmo principio do insertion sort
+        //desenhos podem ajudar a entender oq esta acontecendo
         while(ptr_aux != NULL && ptr_aux->ord < ord){
            o_ptr = ptr_aux;
            ptr_aux = ptr_aux->prox;
         }
 
+
         n_ptr->prox = ptr_aux;
 
-        if(o_ptr ==NULL){
+        if(o_ptr == NULL){
             *ptr = n_ptr;
         } 
         else{
@@ -127,7 +153,7 @@ void inserir(controle **ptr){
     }
     return; 
 }
-
+// printa a linha com todas as informacoes do cadastro
 void imprimir(controle *ptr){
     for(int i = 0; i < 7; i++){
         switch(i){
@@ -157,13 +183,16 @@ void imprimir(controle *ptr){
     putchar('\n');
 }
 
+//ao mesmo tempo que lista toda a lista enumerada, tbm salva no arquivo
 void listar(controle *ptr){
+    //abre arquivo para escrever
+    //ver com a professora ou com outros grupos se os dados anterioves devem
+    //ficar salvos no arquivo "a" ou apagados a cada run "w"
     FILE *fptr;
-
     fptr = fopen("cadastro_de_acesso.txt", "a");
 
     
-
+    //salva e lista todos do controle
     controle *ptr_aux = ptr_inicio;
     if(ptr_aux == NULL){
         p("Lista Vazia!");
@@ -174,9 +203,11 @@ void listar(controle *ptr){
         ptr_aux = ptr_aux->prox;
         p("\n");
     }
-
+    //reseta o ponteiro auxiiar para o inicio da lista
     ptr_aux = ptr_inicio;
 
+    //printa apenas os ponteiros cujo tipo eh visitantes
+    //processo se repete para as outros listas
     p("\nLista de visitantes\n");
     while(ptr_aux != NULL){
         if(!strcmp(ptr_aux->tipo, "Visitante")){
@@ -220,7 +251,7 @@ void listar(controle *ptr){
 
     fclose(fptr);
 }
-
+//olhar aula VII parte 3
 void apagar(controle *ptr_aux){
     if(ptr_aux != NULL){
         while(ptr_aux->prox != NULL){
@@ -233,11 +264,14 @@ void apagar(controle *ptr_aux){
     ptr_inicio = NULL;
 }
 
+
 char *isoficial(char *ptr){
+    //a funcao strstr diz se ptr eh substreng de postos
     if(strstr(postos, ptr)) return "of";
     return "erro";
 }
 
+// mesma estrutura do imprimir porem com a funcao de escrita em arquivo
 void salvar(controle *ptr, FILE *arq){
     char tab = '\t', el = '\n'; 
     for(int i = 0; i < 7; i++){
